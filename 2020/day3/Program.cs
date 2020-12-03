71e1ac4e063969899f9daa4e6339de9c8ec6c0a7
Tyler Pearson <tyler.pearson@thoughtworks.com>
2020-12-03 10:25:31 -0600

    Day 3 part 1

---
+++ 2020/day3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace day3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("data.txt");
+            int trees = 0;
+
+            for(int i = 0; i < lines.Length; i++) {
+                string treeLine = lines[i];
+                int position = i * 3 % treeLine.Length;
+
+                if (treeLine[position] == '#') {
+                    trees++;
+                }
+            }
+
+            Console.WriteLine($"There are {trees} trees.");
+        }
+    }
+}
