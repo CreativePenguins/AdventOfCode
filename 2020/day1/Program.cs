d67a1fe8818e99a2dfa6033296bd89e0f33972b0
Tyler Pearson <tpearson1994@gmail.com>
2020-12-02 18:45:00 -0600

    initial commit

---
+++ 2020/day1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("numbers.txt");
+
+            int num1, num2, num3 = 0;
+
+            for(int i = 0; i < lines.Length; i++) {
+                num1 = int.Parse(lines[i]);
+
+                for(int j = i + 1; j < lines.Length; j++){
+                    num2 = int.Parse(lines[j]);
+
+                    for(int k = j + 1; k < lines.Length; k++){
+                        num3 = int.Parse(lines[k]);
+
+                        if(num1 + num2 + num3 == 2020) {
+                            Console.WriteLine($"The numbers are {num1}, {num2}, and {num3}.");
+                            Console.WriteLine($"Their sum is {num1 * num2 * num3}.");
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+}
