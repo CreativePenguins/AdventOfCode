9a5aaa594dda2823a7eb0c5328fdb9995aa04e5e
Tyler Pearson <tyler.pearson@thoughtworks.com>
2020-12-04 00:41:39 -0600

    Day 4 part 1

---
+++ 2020/day4/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace day4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("data.txt");
+            List<string> requiredFields = new List<string>() {
+                "byr",
+                "iyr",
+                "eyr",
+                "hgt",
+                "hcl",
+                "ecl",
+                "pid"
+            };
+
+            int validPassports = 0;
+
+            List<string> passportFields = new List<string>();
+
+            foreach(string line in lines) {
+                if(line.Trim() == "") {
+                    var hasAllRequiredFields = !requiredFields.Except(passportFields).Any();
+                    
+                    if(hasAllRequiredFields) 
+                        validPassports++;
+                    
+                    passportFields.Clear();
+                } else {
+                    List<string> dataFields = new List<string>(line.Split(' '));
+
+                    dataFields.ForEach(field => {
+                        passportFields.Add(field.Substring(0, 3));
+                    });
+                }
+            }
+
+            Console.WriteLine($"There are {validPassports} valid passports.");
+        }
+    }
+}
