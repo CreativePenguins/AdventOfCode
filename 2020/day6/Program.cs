70a0cc501f0056b105a503a20e46efca6cf8e9d7
Tyler Pearson <tyler.pearson@thoughtworks.com>
2020-12-06 01:10:31 -0600

    Day 6 part 2

--- 2020/day6/Program.cs
+++ 2020/day6/Program.cs
@@ -10,31 +10,62 @@ namespace day6
         static void Main(string[] args)
         {
             string[] lines = File.ReadAllLines("data.txt");
-            List<char> answers = new List<char>();
             int total = 0;
+
+            List<char> answers = new List<char>();
+            Dictionary<char, int> charCount = new Dictionary<char, int>();
+            int people = 0;
 
             foreach(string line in lines) {
                 if(line.Trim() == "") {
-                    total += answers.Count;
-                    answers.Clear();
-                } else if (line.Trim() == lines.Last().Trim()) {
-                    foreach(char c in line.Trim()) {
-                        if(!answers.Contains(c)) {
-                            answers.Add(c);
+                    foreach(char c in answers) {
+                        if(charCount.ContainsKey(c)) {
+                            charCount[c] += 1;
+                        } else {
+                            charCount.Add(c, 1);
                         }
                     }
-                    total += answers.Count;
+
+                    foreach(var entry in charCount) {
+                        if(charCount[entry.Key] == people)
+                            total++;
+                    }
+
+                    people = 0;
                     answers.Clear();
-                } else {
-                    foreach(char c in line.Trim()) {
-                        if(!answers.Contains(c)) {
-                            answers.Add(c);
+                    charCount.Clear();
+                } else if (line.Trim() == lines.Last().Trim()) {
+                    people++;
+                    foreach(char c in line) {
+                        answers.Add(c);
+                    }
+
+                    foreach(char c in answers) {
+                        if(charCount.ContainsKey(c)) {
+                            charCount[c] += 1;
+                        } else {
+                            charCount.Add(c, 1);
                         }
+                    }
+
+                    foreach(var entry in charCount) {
+                        if(charCount[entry.Key] == people)
+                            total++;
+                    }
+
+                    people = 0;
+                    answers.Clear();
+                    charCount.Clear();
+                } else {
+                    people++;
+                    foreach(char c in line) {
+                        answers.Add(c);
                     }
                 }
             }
 
             Console.WriteLine($"Total is {total}"); 
+            // 11902 too high
         }
     }
 }
