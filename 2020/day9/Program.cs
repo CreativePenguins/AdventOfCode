62d2e050a90c82da40019df42d886205c9a773d4
Tyler Pearson <tyler.pearson@thoughtworks.com>
2020-12-09 10:57:36 -0600

    Day 9 part 1

---
+++ 2020/day9/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace day7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ImmutableList<string> input = ImmutableList.Create<string>(File.ReadAllLines("data.txt"));
+
+            for(int i = 25; i < input.Count; i++) {
+                double currentNumber = double.Parse(input[i]);
+                ImmutableList<string> subInput = input.GetRange(i-25, 25);
+
+                bool doesContain = false;
+                
+                foreach(string num in subInput) {
+                    double partner = currentNumber - double.Parse(num);
+                    if(subInput.Contains(partner.ToString())) {
+                        doesContain = true;
+                        break;
+                    } else {
+                        continue;
+                    }
+                }
+
+                if(!doesContain) {
+                    Console.WriteLine($"The number is {currentNumber}");
+                    break;
+                }
+            }
+        }
+    }
+}
